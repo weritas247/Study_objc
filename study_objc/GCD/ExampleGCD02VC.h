e5dbecf09bfdcb62bff259158c8def960c5789bd
LSH <secondwarren@gmail.com>
2019-10-07 20:54:21 +0900

    GCD Example 추가

---
+++ study_objc/GCD/ExampleGCD02VC.h
@@ -0,0 +1,17 @@
+//
+//  ExampleGCD02VC.h
+//  study_objc
+//
+//  Created by MyMacBookPro on 06/10/2019.
+//  Copyright © 2019 Maverick DevStudioa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ExampleGCD02VC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ study_objc/GCD/ExampleGCD02VC.m
@@ -0,0 +1,32 @@
+//
+//  ExampleGCD02VC.m
+//  study_objc
+//
+//  Created by MyMacBookPro on 06/10/2019.
+//  Copyright © 2019 Maverick DevStudioa. All rights reserved.
+//
+
+#import "ExampleGCD02VC.h"
+
+@interface ExampleGCD02VC ()
+
+@end
+
+@implementation ExampleGCD02VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
